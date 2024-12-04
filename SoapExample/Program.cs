78f5a28276d93aa9b74bb80362f36e264284e77f
Johana Elizalde <njelizalde@uce.edu.ec>
2024-12-04 09:44:02 -0500

    Upload Proyect

---
+++ SoapExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ServiceModel;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Configuración del host del servicio
+        Uri baseAddress = new Uri("http://localhost:8000/CalculatorService");
+        using (ServiceHost host = new ServiceHost(typeof(CalculatorService), baseAddress))
+        {
+            host.AddServiceEndpoint(typeof(ICalculator), new BasicHttpBinding(), "");
+            Console.WriteLine("Servicio SOAP escuchando en: " + baseAddress);
+            
+            // Abrir el servicio
+            host.Open();
+            Console.WriteLine("Presiona [Enter] para cerrar el servicio...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SoapExample/SoapService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ServiceModel;
+using System.Xml.Serialization;
+
+// Definir el contrato del servicio SOAP
+[ServiceContract]
+public interface ICalculator
+{
+    [OperationContract]
+    int Add(int num1, int num2);
+
+    [OperationContract]
+    int Subtract(int num1, int num2);
+}
+
+// Implementación del servicio
+public class CalculatorService : ICalculator
+{
+    public int Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    public int Subtract(int num1, int num2)
+    {
+        return num1 - num2;
+    }
+}
